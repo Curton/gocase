3129afb74ca5ff6f4a845a8e4e1211374a6b962f
coveyliu <liukedun@qq.com>
2020-06-03 19:09:21 +0800

    leetcode 66

---
+++ leetcode/66/plus_one.go
@@ -0,0 +1,49 @@
+package _6
+
+// my version
+func PlusOne(digits []int) []int {
+	len := len(digits)
+	carry := false
+	if digits[len-1] == 9 {
+		carry = true
+		digits[len-1] = 0
+	} else {
+		digits[len-1]++
+		return digits
+	}
+
+	for i := len - 2; i > -1; i-- {
+		if carry == false {
+			break
+		}
+		if digits[i] == 9 {
+			digits[i] = 0
+		} else {
+			digits[i]++
+			carry = false
+		}
+	}
+
+	if carry != false {
+		digits[0] = 0
+		return append([]int{1}, digits...)
+	}
+	return digits
+}
+
+// reference
+func plusOne(digits []int) []int {
+	var n int = len(digits)
+	for i := n - 1; i > -1; i-- {
+		if digits[i] < 9 {
+			digits[i]++
+			return digits
+		} else {
+			digits[i] = 0
+		}
+	}
+	var a = make([]int, n+1)
+	a[0] = 1
+	return a
+
+}
---
+++ leetcode/66/plus_one_test.go
@@ -0,0 +1,11 @@
+package _6
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPlusOne(t *testing.T) {
+	digits := []int{9, 9, 9, 9}
+	fmt.Print(PlusOne(digits))
+}
