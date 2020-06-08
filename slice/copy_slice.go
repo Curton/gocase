81ad518ed66b4a1a29c8cbd20721fb7352f55b01
coveyliu <liukedun@qq.com>
2020-06-08 18:45:44 +0800

    update copy slice

--- slice/copy_slice.go
+++ slice/copy_slice.go
@@ -2,6 +2,9 @@ package slice
 
 // use copy()
 func Copy(b []byte) []byte {
+	if b == nil {
+		return nil
+	}
 	c := make([]byte, len(b))
 	copy(c, b)
 	return c
@@ -9,14 +12,24 @@ func Copy(b []byte) []byte {
 
 // append to a nil slice
 func Append(b []byte) []byte {
-	var c []byte
-	c = append(c, b...)
+	if b != nil && len(b) == 0 {
+		return []byte{}
+	}
+	c := append([]byte(nil), b...)
 	return c
 }
 
 // append to a pre-allocated slice
 func Append2(b []byte) []byte {
+	if b == nil {
+		return nil
+	}
 	c := make([]byte, 0, len(b))
 	c = append(c, b...)
+	return c
+}
+
+func Append3(b []byte) []byte {
+	c := append(b[:0:0], b...)
 	return c
 }
--- slice/copy_slice_test.go
+++ slice/copy_slice_test.go
@@ -6,17 +6,30 @@ import (
 )
 
 var text = []byte("ashfdhkjadfbkjbvjfdsbkgbkdfjdsfbhajskdfghkabhlfd")
+var text2 = []byte("")
 
 func TestAppend(t *testing.T) {
-	assert.Equal(t, []byte("ashfdhkjadfbkjbvjfdsbkgbkdfjdsfbhajskdfghkabhlfd"), Append(text), "TestAppendDigitsErr")
+	assert.Equal(t, text, Append(text), "TestAppend Err 1")
+	assert.Equal(t, text2, Append(text2), "TestAppend Err 2")
+	assert.Equal(t, []byte(nil), Append(nil), "TestAppend Err 3")
 }
 
 func TestAppend2(t *testing.T) {
-	assert.Equal(t, []byte("ashfdhkjadfbkjbvjfdsbkgbkdfjdsfbhajskdfghkabhlfd"), Append2(text), "TestAppendDigitsErr")
+	assert.Equal(t, text, Append2(text), "TestAppend2 Err 1")
+	assert.Equal(t, text2, Append2(text2), "TestAppend2 Err 2")
+	assert.Equal(t, []byte(nil), Append2(nil), "TestAppend2 Err 3")
+}
+
+func TestAppend3(t *testing.T) {
+	assert.Equal(t, text, Append3(text), "TestAppend3 Err 1")
+	assert.Equal(t, text2, Append3(text2), "TestAppend3 Err 2")
+	assert.Equal(t, []byte(nil), Append3(nil), "TestAppend3 Err 3")
 }
 
 func TestCopy(t *testing.T) {
-	assert.Equal(t, []byte("ashfdhkjadfbkjbvjfdsbkgbkdfjdsfbhajskdfghkabhlfd"), Copy(text), "CopyDigitsErr")
+	assert.Equal(t, text, Copy(text), "CopyDigits Err")
+	assert.Equal(t, text2, Copy(text2), "CopyDigits Err")
+	assert.Equal(t, []byte(nil), Copy(nil), "CopyDigits Err")
 }
 
 func BenchmarkAppendDigits(b *testing.B) {
@@ -31,6 +44,12 @@ func BenchmarkAppendDigits2(b *testing.B) {
 	}
 }
 
+func BenchmarkAppendDigits3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Append3(text)
+	}
+}
+
 func BenchmarkCopyDigits(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Copy(text)
@@ -41,8 +60,13 @@ func BenchmarkCopyDigits(b *testing.B) {
 goos: darwin
 goarch: amd64
 pkg: gocase/slice
-BenchmarkAppendDigits-4         180540025               33.2 ns/op            48 B/op          1 allocs/op
-BenchmarkAppendDigits2-4        210423793               28.4 ns/op            48 B/op          1 allocs/op
-BenchmarkCopyDigits-4           212710212               28.3 ns/op            48 B/op          1 allocs/op
+BenchmarkAppendDigits
+BenchmarkAppendDigits-16     	34813472	        32.8 ns/op
+BenchmarkAppendDigits2
+BenchmarkAppendDigits2-16    	42832412	        28.2 ns/op
+BenchmarkAppendDigits3
+BenchmarkAppendDigits3-16    	37208007	        32.1 ns/op
+BenchmarkCopyDigits
+BenchmarkCopyDigits-16       	42077460	        28.5 ns/op
 PASS
 */
