2b9ca78bc8afcc756982d3009e5f43f462455689
Covey <covey@liukedun.com>
2020-07-23 12:45:43 +0800

    fuck

---
+++ init/main.go
@@ -0,0 +1,16 @@
+/**
+ * @author Covey Liu, covey@liukedun.com
+ * @date 2020/7/23 12:44
+ */
+
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("This will get called on main initialization")
+}
+
+func main() {
+	fmt.Println("My Wonderful Go Program")
+}
